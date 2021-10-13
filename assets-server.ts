0886e45c27156f624083035c868fc536e5d7953b
Mike Huang <wellwind@mail2000.com.tw>
2021-10-13 23:46:43 +0800

    將 server 改為處理 assets

--- assets-server.ts
+++ assets-server.ts
@@ -2,7 +2,7 @@ import * as connect from 'connect';
 import * as serveStatic from 'serve-static';
 import { join } from 'path';
 
-const distFolder = join(process.cwd(), 'dist/blog/browser');
+const distFolder = join(process.cwd(), 'src');
 connect()
   .use(serveStatic(distFolder))
-  .listen(4200, () => console.log('Server running on 4200...'));
+  .listen(4201, () => console.log('Server running on 4201...'));
