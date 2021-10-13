c77a09b3c794efd4203439acec8b78b3d1597bed
Mike Huang <wellwind@mail2000.com.tw>
2021-10-14 00:16:10 +0800

    serve static content for prerender

--- src/app/blog/blog-post/blog-post.component.ts
+++ src/app/blog/blog-post/blog-post.component.ts
@@ -4,6 +4,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { filter, map, switchMap, tap } from 'rxjs/operators';
 import * as markdownIt from 'markdown-it';
+import { environment } from '../../../environments/environment';
 
 const markdown = markdownIt();
 
@@ -28,8 +29,9 @@ export class BlogPostComponent implements OnInit {
   }
 
   private getMarkdownContent(slug: string) {
+    console.log(`${environment.url}assets/blog/${slug}/${slug}.md`);
     return this.httpClient
-      .get(`http://localhost:4200/assets/blog/${slug}/${slug}.md`, { responseType: 'text' })
+      .get(`${environment.url}assets/blog/${slug}/${slug}.md`, { responseType: 'text' })
       .pipe(
         map(content => markdown.render(content)),
         map(content => content.replace(/\{% asset_img (.*) (.*)%\}/g, `<img src="./assets/blog/${slug}/$1" alt="$2" />`)),
